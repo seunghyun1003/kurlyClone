35ed439cde0566c7c6f58ce234978f5a26a01138
seunghyun1003 <s961003-@naver.com>
2021-07-19 17:17:04 +0900

    make components/searchrequire.vue frame

--- src/components/SearchRequire.vue
+++ src/components/SearchRequire.vue
@@ -1,5 +1,15 @@
 <template>
-  <div id="searchrequire">searchrequire</div>
+  <div id="searchrequire">
+    <div class="search-form-title">검색조건</div>
+    <div class="search-form-input">
+      <b-form-input placeholder="Search" id="input-default"></b-form-input>
+    </div>
+    <div class="search-form-button">
+      <button>
+        검색하기
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,8 +17,39 @@ export default {
   name: "SearchRequire",
   data() {},
   methods: {}
-}
+};
 </script>
 
 <style>
+#searchrequire {
+  display: flex;
+  width: 100%;
+  border-top: 2px solid purple;
+  border-bottom: 2px solid purple;
+  padding: 1.5em;
+  justify-content: space-evenly;
+  margin-bottom: 3em;
+}
+.search-form-title {
+  flex-basis: 10%;
+  line-height: 2.375em;
+  text-align: left;
+  font-weight: 600;
+}
+.search-form-input {
+  flex-basis: 60%;
+}
+.search-form-input > input[type="text"] {
+  width: 100%;
+}
+.search-form-button {
+  flex-basis: 18%;
+}
+.search-form-button > button {
+  width: 100%;
+  height: 2.375em;
+  background-color: purple;
+  color: white;
+  font-weight: 600;
+}
 </style>
--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -1,22 +1,34 @@
 <template>
   <div id="searchpage">
-    검색페이지
+    <div class="second-title">
+      상품검색
+    </div>
     <SearchRequire></SearchRequire>
     <SearchResult></SearchResult>
   </div>
 </template>
 
 <script>
-import SearchRequire from "@/components/SearchRequire.vue"
-import SearchResult from "@/components/SearchResult.vue"
+import SearchRequire from "@/components/SearchRequire.vue";
+import SearchResult from "@/components/SearchResult.vue";
 export default {
   name: "SearchPage",
   components: {
     SearchRequire,
-    SearchResult,
-  },
-}
+    SearchResult
+  }
+};
 </script>
 
 <style>
+#searchpage {
+  width: 1000px;
+  margin: 0 auto;
+}
+.second-title {
+  font-size: 1.6em;
+  font-weight: bold;
+  line-height: 4em;
+  height: 4em;
+}
 </style>
